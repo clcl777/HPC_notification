15f84f8967e3121ff38858235095aa4944582b01
clcl777 <77223796+clcl777@users.noreply.github.com>
2023-03-29 18:10:28 +0900

    basic認証実装

--- server2/app/controllers/profiles_controller.rb
+++ server2/app/controllers/profiles_controller.rb
@@ -1,3 +1,15 @@
 class ProfilesController < ApplicationController
-  def index; end
+  before_action :basic_auth
+  BASIC_AUTH_NAME = '123'
+  BASIC_AUTH_PASSWORD = '123'
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == BASIC_AUTH_NAME && password == BASIC_AUTH_PASSWORD
+    end
+  end
+
+  def index
+    @users_profiles = UsersProfile.all
+  end
 end

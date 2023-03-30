0be496ec46c159719e7f4c6476587f2abbfa258d
clcl777 <77223796+clcl777@users.noreply.github.com>
2023-03-30 17:36:06 +0900

    server1実装

---
+++ server1/bat/create.bat
@@ -0,0 +1,2 @@
+call C:\ProgramData\Anaconda3\Scripts\activate.bat
+echo y|call conda create --prefix ../env python flask requests
---
+++ server1/example/test.bat
@@ -0,0 +1,2 @@
+@rem http://localhost:5050/notification/{id}　idが1の場合は以下のようになる
+curl http://localhost:5050/notification/1

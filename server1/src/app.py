c0736adf608a01c99fcc4d8108bb833e3a3de43d
clcl777 <77223796+clcl777@users.noreply.github.com>
2023-08-03 21:50:26 +0900

    バグ修正

--- server1/src/app.py
+++ server1/src/app.py
@@ -13,7 +13,7 @@ def home():
     return "It works!"
 
 
-@app.route("/notification/<int:id>/<int:task_id>/<str:task_name>/<str:used_node>")
+@app.route("/notification/<int:id>/<int:task_id>/<string:task_name>/<string:used_node>")
 def notification(id, task_id, task_name, used_node):
     URL = URL_SERVER2 + "/notification/" + str(id) + "/" + str(task_id) + "/" + task_name + "/" + used_node
     response = requests.get(URL)

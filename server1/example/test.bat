7ac36ed4f0cede1125706a9820a27ed9079187d7
clcl777 <77223796+clcl777@users.noreply.github.com>
2023-08-03 21:51:06 +0900

    タスク詳細更新

--- server1/example/test.bat
+++ server1/example/test.bat
@@ -1,2 +1,2 @@
-@rem http://localhost:5050/notification/{id}　idが1の場合は以下のようになる
-curl http://localhost:5050/notification/1
+@rem http://localhost:5050/notification/{id}/%CCP_JOBID%/%CCP_JOBNAME%/%COMPUTERNAME%　idが1の場合は以下のようになる
+curl http://localhost:5050/notification/1/%CCP_JOBID%/%CCP_JOBNAME%/%COMPUTERNAME%

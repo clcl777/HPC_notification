3f7fce91b7ff2262cbdca47554b34d215f6bb07c
clcl777 <77223796+clcl777@users.noreply.github.com>
2023-08-04 10:43:37 +0900

    変数名変更

--- server2/config/routes.rb
+++ server2/config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'profiles/index'
   get 'home' => 'home#top'
   post 'callback' => 'line_bot#callback'
-  get 'notification/:id/:task_id/:task_name/:used_node' => 'line_bot#notification'
+  get 'notification/:id/:job_id/:job_name/:computer_name' => 'line_bot#notification'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
